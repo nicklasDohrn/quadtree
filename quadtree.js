3f8815b39f60da46525c84e0e4e36df044ef9a4e
Nicklas Dohrn <nicklas.dohrn@gmail.com>
2019-05-24 12:23:43 +0000

    added Quadtree

--- quadtree.js
+++ quadtree.js
@@ -31,18 +31,40 @@ var Quadtree = function (x, y, width, height) {
 	    this.point = point;
 	else if(this.childs == null) {
 	    for(var i = 0; i < 4; i++) {
-		this.childs.push(new Quadtree(this.x + 
+		this.childs = [];
+		this.childs.push(new Quadtree(this.x + (this.width / 2 * (i % 2)),
+					      this.y + (this.height / 2 *((i/2)%2)),
+					      this.width - (this.width / 2 * ((i+1) % 2)),
+					      this.height - (this.height / 2 * (((i/2) + 1) %2))));
+		}
+		this.addPoint(point);
+		this.addPoint(this.point);
+	}
+	else {
+	    if (point.y < this.height / 2){
+		if (point.x < this.width)
+		    this.childs[0].addPoint(point);
+		else
+		    this.childs[1].addPoint(point);
+	    } else {
+		if(point.x < this.width)
+		    this.childs[2].addPoint(point);
+		else
+		    this.childs[3].addPoint(point);
 	    }
 	}
     }
 }
 
 
+var tree = new Quadtree(0,0,canvas.width,canvas.height);
+tree.addPoint(new Point(25,25));
+tree.addPoint(new Point(300,25));
+tree.addPoint(new Point(475,25));
+
 for(var i = 0; i < 100; i++) {
     points.push(new Point(parseInt(Math.random()*canvas.width),parseInt(Math.random()*canvas.width)));
 }
-points.push(new Point(3,3));
-points.push(new Point(3,3));
 draw();
 
 
